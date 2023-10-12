044ba9a94dec41bf33e7e62ba79e97fb83cd8571
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-10-12 12:35:48 +0800

    add: trans field to queue model class

--- lib/model/now_serving_model.dart
+++ lib/model/now_serving_model.dart
@@ -2,20 +2,24 @@ class NowServing {
   NowServing({
     required this.visitor,
     required this.window,
+    required this.trans,
   });
 
   NowServing.fromJson(dynamic json) {
     visitor = json['visitor'];
     window = json['window'];
+    trans = json['trans'];
   }
 
   String? visitor;
   int? window;
+  String? trans;
 
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['visitor'] = visitor;
     map['window'] = window;
+    map['trans'] = trans;
     return map;
   }
 }
