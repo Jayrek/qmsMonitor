a6115f8a2beef0ff56d33fbadf80e5e28d3b0ed3
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-10-12 12:35:27 +0800

    update: base url

--- lib/constant_util.dart
+++ lib/constant_util.dart
@@ -1,5 +1,6 @@
 class ConstantUtil{
   ConstantUtil._();
 
-  static const String baseUrl = 'https://demo.myhris.info/qms/api/';
+  static const String baseUrl = 'https://demo.myhris.info/qms/api_new/';
+  // static const String baseUrl = 'https://demo.myhris.info/qms/api/';
 }
